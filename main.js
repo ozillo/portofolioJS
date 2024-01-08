97f1401dc1bd7509b520e5ae1a7890a3ea1d20f3
ozillo <marcmateo25@gmail.com>
2024-01-08 11:07:19 +0100

    import NavBar

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import "./style.css";
-import { changeTheme } from "./components/Navbar/Navbar";
+//import { changeTheme } from "./components/Navbar/Navbar";
 import { linkPage } from "./utils/linkPage";
-import { Navbar } from "./components/Navbar/Navbar";
+//import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
 import { Home } from "./pages/Home/Home";
 import { Projects } from "./pages/Projects/Projects";
 import { Divider } from "./components/Divider/Divider";
-
+import { Navbar, changeTheme } from "./components/Navbar/Navbar";
 const header = document.querySelector("header");
 header.innerHTML = Navbar();
 const footer = document.querySelector("footer");
